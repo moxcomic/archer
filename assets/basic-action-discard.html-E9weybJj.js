db16d0c59fac00d1fbe59388dd538d49962a4956
moxcomic <37604141+moxcomic@users.noreply.github.com>
2024-09-08 04:43:08 +0800

    update

---
+++ assets/basic-action-discard.html-E9weybJj.js
@@ -0,0 +1,34 @@
+import{_ as a,c as s,a as e,o as i}from"./app-CKvRblde.js";const l={};function c(r,n){return i(),s("div",null,n[0]||(n[0]=[e(`<h1 id="调用接口" tabindex="-1"><a class="header-anchor" href="#调用接口"><span>调用接口</span></a></h1><ul><li>使用 <a href="/zh/auto/majsoul/actions/basic-action-majsoul" target="_blank" rel="noopener noreferrer">MajsoulActions</a> 包中的 <code>Discard</code> 方法来弃牌。</li></ul><h1 id="示例代码" tabindex="-1"><a class="header-anchor" href="#示例代码"><span>示例代码</span></a></h1><div class="language-golang line-numbers-mode" data-highlighter="prismjs" data-ext="golang" data-title="golang"><pre><code><span class="line">package main</span>
+<span class="line"></span>
+<span class="line">import (</span>
+<span class="line">  &quot;github.com/moxcomic/Archer/actions&quot;</span>
+<span class="line">  &quot;github.com/moxcomic/lq&quot;</span>
+<span class="line">)</span>
+<span class="line"></span>
+<span class="line">const (</span>
+<span class="line">  E_PlayOperation_None           = iota</span>
+<span class="line">  E_PlayOperation_Discard</span>
+<span class="line">  E_PlayOperation_Chi</span>
+<span class="line">  E_PlayOperation_Pon</span>
+<span class="line">  E_PlayOperation_Ankan</span>
+<span class="line">  E_PlayOperation_Minkan</span>
+<span class="line">  E_PlayOperation_Kakan</span>
+<span class="line">  E_PlayOperation_RiiChi</span>
+<span class="line">  E_PlayOperation_Tsumo</span>
+<span class="line">  E_PlayOperation_Ron</span>
+<span class="line">  E_PlayOperation_JiuZhongJiuPai</span>
+<span class="line">  E_PlayOperation_Babei</span>
+<span class="line">  E_PlayOperation_HuanSanZhang</span>
+<span class="line">  E_PlayOperation_DingQue</span>
+<span class="line">  E_PlayOperation_Reveal</span>
+<span class="line">  E_PlayOperation_Unveil</span>
+<span class="line">  E_PlayOperation_LockTile</span>
+<span class="line">  E_PlayOperation_Revealliqi</span>
+<span class="line">)</span>
+<span class="line"></span>
+<span class="line">func onExecute(result *lq.NotifyAIResult, risk []float64, r, m, f, t float64) {</span>
+<span class="line">  if result.GetResult()[0].GetType() == E_PlayOperation_Discard {</span>
+<span class="line">    actions.NewMajsoulActions().Discard(result.GetResult()[0].GetTile(), result.GetResult()[0].GetMoqie())</span>
+<span class="line">  }</span>
+<span class="line">}</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li>在 <code>onExecute</code> 函数中，我们通过 <code>GetResult</code> 方法获取到当前出牌的类型，如果是弃牌，我们调用 <code>Discard</code> 方法来弃掉相应的牌。</li><li>当然这里只是一个示例，实际使用时，你可能需要根据实际情况来修改代码。</li><li><code>lq.NotifyAIResult</code> 参考 <a href="/zh/auto/majsoul/lq/basic-lq" target="_blank" rel="noopener noreferrer">NotifyAIResult</a></li><li><code>GetType</code> 参考 <a href="/zh/auto/majsoul/action_type/basic-action-type" target="_blank" rel="noopener noreferrer">动作类型</a></li></ul>`,5)]))}const o=a(l,[["render",c],["__file","basic-action-discard.html.vue"]]),d=JSON.parse('{"path":"/zh/auto/majsoul/action/basic-action-discard.html","title":"基础","lang":"zh-CN","frontmatter":{"title":"基础"},"headers":[],"git":{},"filePathRelative":"zh/auto/majsoul/action/basic-action-discard.md"}');export{o as comp,d as data};
