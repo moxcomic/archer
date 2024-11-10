9d54b653b1b60bf0d3e82a17ed45358a218fb3b4
moxcomic <37604141+moxcomic@users.noreply.github.com>
2024-11-10 21:57:35 +0800

    update

---
+++ assets/basic-framework.html-BgZsB2Pq.js
@@ -0,0 +1,24 @@
+import{_ as s,c as a,a as e,o as i}from"./app-BzXfoLuy.js";const l={};function c(d,n){return i(),a("div",null,n[0]||(n[0]=[e(`<h1 id="最小化框架" tabindex="-1"><a class="header-anchor" href="#最小化框架"><span>最小化框架</span></a></h1><div class="language-golang line-numbers-mode" data-highlighter="prismjs" data-ext="golang" data-title="golang"><pre><code><span class="line">package main</span>
+<span class="line"></span>
+<span class="line">import (</span>
+<span class="line">	&quot;github.com/moxcomic/lq&quot;</span>
+<span class="line">)</span>
+<span class="line"></span>
+<span class="line">func onLogin() {}</span>
+<span class="line"></span>
+<span class="line">func onRoomGameStart() {}</span>
+<span class="line"></span>
+<span class="line">func onMatchGameStart() {}</span>
+<span class="line"></span>
+<span class="line">func onNewRound() {}</span>
+<span class="line"></span>
+<span class="line">func onRoundEnd() {}</span>
+<span class="line"></span>
+<span class="line">func onPlayerLeaving(seat int) {}</span>
+<span class="line"></span>
+<span class="line">func onExecute(result *lq.NotifyAIResult, risk []float64, rating, matches, fivePer, tenPer float64) {}</span>
+<span class="line"></span>
+<span class="line">func onGameEnd() {}</span>
+<span class="line"></span>
+<span class="line">func onServerDisconnect() {}</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>该框架包含了最小化的游戏逻辑, 只包含了一些基本的事件, 你可以根据自己的需求进行修改和扩展</p>`,3)]))}const t=s(l,[["render",c],["__file","basic-framework.html.vue"]]),p=JSON.parse('{"path":"/zh/auto/basic-framework.html","title":"基础框架","lang":"zh-CN","frontmatter":{"title":"基础框架"},"headers":[],"git":{},"filePathRelative":"zh/auto/basic-framework.md"}');export{t as comp,p as data};
